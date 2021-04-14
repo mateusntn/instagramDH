c5224b5fc17b4528b0e324d5faa1c9b02e888056
mateusntn <mateusantoniodev@gmail.com>
2021-04-14 11:10:45 -0300

    Creates the postController

---
+++ controllers/postsController.js
@@ -0,0 +1,27 @@
+const { Post, sequelize} = require('../models/');
+
+const postsController = {
+    index: async (req, res) => {
+        const posts = await Post.findAll();
+        return res.json(posts);
+    },
+    create: async (req, res) => {
+        const {texto, img, usuarios_id, n_likes} = req.body;
+        const novoPost = await Post.create({texto, img, usuarios_id, n_likes});
+        return res.json(novoPost);
+    },
+    update: async (req, res) => {
+        const {id} = req.params;
+        const {texto, img, usuarios_id, n_likes} = req.body;
+        const postAtualizado = await Post.update({texto, img, usuarios_id, n_likes}, {where: {id}})
+        return res.json(postAtualizado);
+    },
+    delete: async (req, res) => {
+        const {id} = req.params;
+        const postDeletado = await Post.destroy({where: {id}});
+        return res.json(postDeletado);
+    }
+        
+}
+
+module.exports = postsController;
