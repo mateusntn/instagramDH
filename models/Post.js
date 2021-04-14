8dc1c1e2b5f65e847a5a6f7e61a1338a2e876570
mateusntn <mateusantoniodev@gmail.com>
2021-04-14 11:11:34 -0300

    Creates the post model

---
+++ models/Post.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define(
+        'Post',{
+            texto: DataTypes.STRING,
+            img: DataTypes.STRING,
+            usuarios_id: DataTypes.INTEGER,
+            n_likes : DataTypes.INTEGER
+        }, {
+            tableName: "posts",
+            timestamps: false
+        }
+    );
+
+    return Post;
+}
