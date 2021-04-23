266e7cc90a7d01c50701e4838ee5fd07d451258f
mateusntn <mateusantoniodev@gmail.com>
2021-04-23 09:54:53 -0300

    Creates model Comentario

---
+++ models/Comentario.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Comentario = sequelize.define(
+        "Comentario", {
+            texto: DataTypes.STRING,
+            usuarios_id: DataTypes.INTEGER,
+            posts_id: DataTypes.INTEGER,
+        }, {
+            tableName: "comentarios",
+            timestamps: false
+        }
+    );
+
+    Comentario.associate = (models) => {
+        Comentario.belongsTo(models.Post, { as: "post", foreignKey: "posts_id"})
+    }
+
+    return Comentario;
+
+}
